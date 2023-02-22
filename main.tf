e12b8835fe92d2f42ad6028bdca49813859dbe2a
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-02-22 09:17:43 +0100

    docs: document the minimal curl version required

--- main.tf
+++ main.tf
@@ -139,6 +139,7 @@ resource "argocd_application" "this" {
 
 resource "null_resource" "wait_for_keycloak" {
   # Until curl successfully completes the requested transfer, wait 10 seconds and retry for 180 seconds until timeout.
+  # --retry-all-errors makes curl retry even on non-transient HTTP errors (e.g. 404), requires curl >= 7.71.0
   # -f makes curl fail on server errors
   # -s prevents it from printing messages and the progress meter
   # -o /dev/null discards the output message
