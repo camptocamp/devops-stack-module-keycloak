1c21bdd18db8d3efc5d969088e77a1ae5469cd3c
Armand LEOPOLD <armand.leopold@outlook.com>
2023-08-12 15:40:43 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -15,10 +15,10 @@ locals {
         username = base64encode("postgres")
         password = base64encode(random_password.db_password.0.result)
         host     = "keycloak-postgres-db"
-        pvc = [{
+        pvc = {
           enabled = false
           size    = "1Gi"
-        }]
+        }
       }
       ingress = {
         enabled = true
